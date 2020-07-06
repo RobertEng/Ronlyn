ed4ddb510829ae3d2d0fd065e947a4bd059c4161
Robert Eng <googlypantsroberteng@gmail.com>
2020-07-06 07:41:06 -0700

    Examples folder

---
+++ examples/data.tsx
@@ -0,0 +1,25 @@
+const section1 = {fragment: "I pour a cup of hot", fragmentType: "hardcoded"}
+const section2 = {fragment: "coffee", fragmentType: "fill-in"}
+const fillIn1: Clause[] = [section1, section2]
+
+const s1 = {fragment: "I also eat <span className='red'>oatmeal<span> with berries like", fragmentType: "hardcoded"}
+const s2 = {fragment: "blueberries", fragmentType: "fill-in"}
+const s3 = {fragment: "and", fragmentType: "hardcoded"}
+const s2 = {fragment: "strawberries", fragmentType: "fill-in"}
+const fillIn2: Clause[] = [s1, s2, s3, s4]
+
+
+  {
+    title: "For breakfast",
+    fillIns: [
+      [
+
+      ],
+      [
+
+      ],
+    ],
+}
+
+
+I pour a cup of hot %coffee%.
